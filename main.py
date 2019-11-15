8e58b202b1ce97acf26e757cf2f36dbd72f6415e
Phillip <sxq456@my.utsa.edu>
2019-11-14 22:19:23 -0600

    Created function to get tag/index/offset values

--- main.py
+++ main.py
@@ -2,7 +2,34 @@ import os
 import sys
 import math
 import argparse
-#from parser.py import parseTraceFile
+
+#==============================
+#FUNCTIONS
+#==============================
+def calculate_address_space(address, tag_bits, index_bits, block_offset_bits):
+    #convert hex string to binary string
+    address_binary = bin(int(address, 16))[2:].zfill(32)
+    #get the block offset, tag and index in binary
+    block_offset_bin = address_binary[(32 - block_offset_bits):]
+    index_bin = address_binary[(32 - block_offset_bits - index_bits):(32 - block_offset_bits)]
+    tag_bin = address_binary[:(tag_bits)]
+    #print("Tag in binary: " + tag_bin)
+    #print("Index in binary: " + index_bin)
+    #print("Block Offset in binary: " + block_offset_bin)
+    #convert the binary values
+    #Block offset as an int
+    block_offset = str(int(block_offset_bin, 2))
+    #index as an int
+    index = str(int(index_bin, 2))
+    #tag needs to be hex string
+    tag = str(hex(int(tag_bin, 2)))
+    #create a list of all the items
+    result = [tag, index, block_offset]
+    return result
+
+#==============================
+#MAIN PROGRAM
+#==============================
 #Check for proper usage
 if len(sys.argv) != 11:
     print ("Usage: " + sys.argv[0] + " –f <trace file name> –s <cache size in KB> –b <block size> –a <associativity> –r <replacement policy>")
@@ -107,12 +134,16 @@ for line in f:
     if new_block:
         bytes_read = int(tokens[1][1:3])#element2(length), numbers 2 and 3
         i_address = int(tokens[2], 16)
+        hex_address = str(tokens[2])
         #print("Address: 0x%s length=%d byte(s)." % (tokens[2], bytes_read), end=' ')
         new_block = False
-        #This converts the address from hex to binary. This allows us to get the index/tag/offset
-        address_binary = bin(int(i_address, 16))[2:].zfill(32)
+        #get the tag, the index and the block offset
+        print("Address to be calculated:" + hex_address)
+        address_space = calculate_address_space(hex_address, int(tag_size), int(index_size), int(offset_size))#all sizes are in bits
+        print(address_space)#TEST remove later
         
-        #insert the adress into the cache
+        #TODO 
+        # insert the adress into the cache
         
         
         #milestone 1 code
