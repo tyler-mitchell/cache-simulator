d5e97c1c0130857100605065a8e71c2898b0ede6
Phillip <sxq456@my.utsa.edu>
2019-11-19 13:43:20 -0600

    insert_address works. Trying to get other 2 addresses
    Now that the function works correctly we can update the cache with any address, assuming we can get the other addresses.
    I'm working on inserting the write address from src.

--- cache.py
+++ cache.py
@@ -60,13 +60,92 @@ class Cache:
         # 1 - miss rate = hit rate
         self.hit_rate = (1 - miss_rate) * 100
 
-    def simulate_cache(self):
+    def insert_address(self, address_space, bytes_read):
         tag_size = self.tag_size
         index_size = self.index_size
         trace_file = self.trace_file
         index_list = self.index_list
         block_size = self.block_size
+        cache_hit = False
+        print("Data to be inserted into cache block")
+        print("Index:" + str(address_space['index']))
+        print("Tag:" + address_space['tag'])
 
+        # HANDLING ROLLOVER INTO OTHER ROWS                # get the number of rows to be read
+        offset = address_space['block_offset']  # get the offset
+        index_rollover = int((int(offset) + bytes_read) / block_size)
+        if((int(offset) + bytes_read % block_size) != 0):  # if there is a remainder
+            index_rollover += 1  # add a row to be read
+            
+        # Sometimes our offset and read size will "rollover" into other rows and their blocks
+        for rollover in range(0, index_rollover):
+            # get the correct row to check
+            current_index = rollover + address_space['index']
+            #This handles going beyond the cache index for a rollover
+            if (current_index + 1) >= self.indices:
+                #go to the first index
+                current_index = current_index - self.indices
+            #print("rows in the cache: " + str(self.indices))
+            print("Rollover: " + str(rollover))
+            #print("Base Index: " + str(address_space['index']))
+            #print("current index: " + str(current_index))
+
+            # Given the tag and index, try to find the block that matches this tag in the set at this index
+            # check this row's blocks for a match
+            for cache_block in index_list[current_index]:
+                if cache_block.tag == address_space['tag']:
+                    cache_hit = True  # the block has been found!
+
+            # NO MATCHING TAG FOUND, BEGIN REPLACEMENT
+            if (cache_hit == False):
+                empty_block = False
+                self.cache_miss_count += 1  # This counts as a miss
+                # find an empty block to replace
+                for cache_block in index_list[address_space['index']]:
+                    if (cache_block.valid == 0):
+                        # empty block found, fill in the tag
+                        #print("Empty block found")
+                        cache_block.valid = 1  # the block now has a tag
+                        empty_block = True  # No need for replacement algorithm if empty block was found
+                        break  # exit the loop and get out of this set
+
+                # if no empty block was found, engage the replacement algorithm
+                if empty_block == False:
+                    print("Executing replacement algorithm")
+                    # rr
+                    if(self.replacement_policy == "RR"):
+                        first_time = True
+                        count = 0
+                        for cache_block in index_list[current_index]:
+                            if cache_block.next == True:
+                                # replace
+                                cache_block.tag = address_space['tag']
+                                cache_block.next = False
+                                first_time = False
+                                # update next block
+                                # if not the last block, update the next one
+                                if (count + 1) < self.associativity:#check that the next block isn't out of range(remember 0 indexing)
+                                    index_list[current_index][(count + 1)].next = True
+                                else:
+                                    index_list[current_index][0].next = True
+                            count += 1
+
+                        if(first_time == True):
+                            # set first block to next
+                            index_list[current_index][0].next = True
+                    elif(self.replacement_policy == "RND"):
+                        # For the Random algorithm just pick a block index at random. Literally get a value and go to
+                        # index_list[address_space['index']][RANDOM NUMBER GOES HERE].tag = new tag
+                        #Assuming no blocks are empty, which is how we got to this point
+                        #get a random number in the range of 0 to our associativity.
+                        random_num = random.randrange(0,self.associativity)
+                        index_list[current_index][random_num].tag = address_space['tag']
+                        print("Block " + str(random_num) + " replaced with " + address_space['tag'])
+
+    def simulate_cache(self):
+        tag_size = self.tag_size
+        index_size = self.index_size
+        trace_file = self.trace_file
         self.build_cache()
 
         new_block = True
@@ -86,88 +165,10 @@ class Cache:
                 # get the tag, the index and the block offset
                 address_space = self.calculate_address_space(hex_address, int(tag_size), int(
                     index_size), int(self.offset_size))  # all sizes are in bits
-                #new function to insert addresses
-                #self.insert_address(self,address_space)
+                #put the address in the cache
+                self.insert_address(address_space, bytes_read)
 
-                #======================================================================================
-                cache_hit = False
-
-                print("Data to be inserted into cache block")
-                print("Index:" + str(address_space['index']))
-                print("Tag:" + address_space['tag'])
-
-                # HANDLING ROLLOVER INTO OTHER ROWS                # get the number of rows to be read
-                offset = address_space['block_offset']  # get the offset
-                index_rollover = int((int(offset) + bytes_read) / block_size)
-                if((int(offset) + bytes_read % block_size) != 0):  # if there is a remainder
-                    index_rollover += 1  # add a row to be read
-
-                # Sometimes our offset and read size will "rollover" into other rows and their blocks
-                for rollover in range(0, index_rollover):
-                    # get the correct row to check
-                    current_index = rollover + address_space['index']
-                    #This handles going beyond the cache index for a rollover
-                    if (current_index + 1) >= self.indices:
-                        #go to the first index
-                        current_index = current_index - self.indices
-                    #print("rows in the cache: " + str(self.indices))
-                    print("Rollover: " + str(rollover))
-                    #print("Base Index: " + str(address_space['index']))
-                    #print("current index: " + str(current_index))
-
-                    # Given the tag and index, try to find the block that matches this tag in the set at this index
-                    # check this row's blocks for a match
-                    for cache_block in index_list[current_index]:
-                        if cache_block.tag == address_space['tag']:
-                            cache_hit = True  # the block has been found!
-
-                    # NO MATCHING TAG FOUND, BEGIN REPLACEMENT
-                    if (cache_hit == False):
-                        empty_block = False
-                        self.cache_miss_count += 1  # This counts as a miss
-                        # find an empty block to replace
-                        for cache_block in index_list[address_space['index']]:
-                            if (cache_block.valid == 0):
-                                # empty block found, fill in the tag
-                                #print("Empty block found")
-                                cache_block.valid = 1  # the block now has a tag
-                                empty_block = True  # No need for replacement algorithm if empty block was found
-                                break  # exit the loop and get out of this set
-
-                        # if no empty block was found, engage the replacement algorithm
-                        if empty_block == False:
-                            print("Executing replacement algorithm")
-                            # rr
-                            if(self.replacement_policy == "RR"):
-                                first_time = True
-                                count = 0
-                                for cache_block in index_list[current_index]:
-                                    if cache_block.next == True:
-                                        # replace
-                                        cache_block.tag = address_space['tag']
-                                        cache_block.next = False
-                                        first_time = False
-                                        # update next block
-                                        # if not the last block, update the next one
-                                        if (count + 1) < self.associativity:#check that the next block isn't out of range(remember 0 indexing)
-                                            index_list[current_index][(count + 1)].next = True
-                                        else:
-                                            index_list[current_index][0].next = True
-                                    count += 1
-
-                                if(first_time == True):
-                                    # set first block to next
-                                    index_list[current_index][0].next = True
-                            elif(self.replacement_policy == "RND"):
-                                # For the Random algorithm just pick a block index at random. Literally get a value and go to
-                                # index_list[address_space['index']][RANDOM NUMBER GOES HERE].tag = new tag
-                                #Assuming no blocks are empty, which is how we got to this point
-                                #get a random number in the range of 0 to our associativity.
-                                random_num = random.randrange(0,self.associativity)
-                                print("Random number chosen: " + str(random_num))
-                                index_list[current_index][random_num].tag = address_space['tag']
-                                print("Block " + str(random_num) + " replaced with " + address_space['tag'])
-            #=======================================================================================================================
+                
             # Read the second line
             else:
                 w_address = hex(int(tokens[1], 16))
@@ -177,6 +178,13 @@ class Cache:
                 r_msg = "Data read at %s, length = 4 bytes" % (
                     r_address) if int(r_address, 16) else "No data reads."
                 rw_msg = "%s %s" % (w_msg, r_msg)
+                #grab the write address if it's not 0
+                if (w_address != 0):
+                    address_space = self.calculate_address_space(str(tokens[1]), int(tag_size), int(index_size), int(self.offset_size))
+                    print("Address to be inserted: ")
+                    print(address_space)
+                    #self.insert_address(address_space, 4)
+                
                 # TODO
                 # Check if the data in the src/dst is 0.
                 # If it is, ignore it.
@@ -191,10 +199,10 @@ class Cache:
         self.calculate_hit_rate()
 
     def display_cache(self):
-        # for count, row in enumerate(self.index_list):
-        #     for column in row:
-        #         print("Row #" + str(count) + ", valid bit:" + str(column.valid) +
-        #               ", tag:" + str(column.tag) + ", time since last use value:" + str(column.timeSinceLastUse))
+        #for count, row in enumerate(self.index_list):
+        #    for column in row:
+        #        print("Row #" + str(count) + ", valid bit:" + str(column.valid) + 
+        #            ", tag:" + str(column.tag))
 
         print("Cache misses:" + str(self.cache_miss_count))
         print("Lines read:" + str(self.total_cycles))
